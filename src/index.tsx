6b9807da2b1023a77bc5dd42db9cc1fbc977bad0
AndrewVKerr2001 <148022069+AndrewVKerr2001@users.noreply.github.com>
2024-11-24 14:29:35 -0600

    Added tailwind & typescript, removed template files.

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Header from './components/header/index.tsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as ReactDOM.Container);
+root.render(
+  <React.StrictMode>
+    <Header/>
+    <main>
+      
+    </main>
+  </React.StrictMode>
+);
